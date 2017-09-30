3ce46fcb473e51b8f93e49a6b151a1dee211484f
Daniel Roman <roman.martinez.daniel@gmail.com>
2017-09-30 20:16:43 +0200

    First Commit

---
+++ resources/views/front/home.blade.php
@@ -0,0 +1,12 @@
+@extends('front.masterlayout')
+
+@section('front_title', 'Home')
+
+@section('front_content')
+
+<div class="starter-template">
+	<h1>Bootstrap starter template</h1>
+	<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+</div>
+
+@endsection
